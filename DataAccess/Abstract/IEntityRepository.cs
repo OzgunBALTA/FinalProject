535e0f0e3f30e69df8b5385f722a46e4178d5468
ozgun balta <ozgunbalta@Icloud.com>
2022-05-04 16:33:16 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T> where T : class, IEntity,new() // Generic consraint (Kısıtlama) Sadece IEntity referansları yazılabilir. New() ile IEntity'i de eleriz -interface new'lenemez-.
+    {
+        //DataAccess operasyonlarında ortak olacak kodlar
+        List<T> GetAll(Expression<Func<T,bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
